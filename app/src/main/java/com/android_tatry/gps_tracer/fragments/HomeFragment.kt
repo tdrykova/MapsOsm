41cd075f8991e619fb8c512476d08bffd8ab39f5
tdrykova <tdrykova@edu.hse.ru>
2023-10-27 11:06:16 +0300

    init osm

--- app/src/main/java/com/android_tatry/gps_tracer/fragments/HomeFragment.kt
+++ app/src/main/java/com/android_tatry/gps_tracer/fragments/HomeFragment.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment
 import com.android_tatry.gps_tracer.databinding.FragmentHomeBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.library.BuildConfig
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
@@ -23,12 +25,29 @@ class HomeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initOsm()
+    }
+
     private fun settingsOsm() {
         Configuration.getInstance().load(
             activity as AppCompatActivity,
             activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
         )
         Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
+    }
+
+    private fun initOsm() = with(binding){
+        map.controller.setZoom(20.0)
+        val mLocProvider = GpsMyLocationProvider(activity)
+        val mLocOverlay = MyLocationNewOverlay(mLocProvider, map)
+        mLocOverlay.enableMyLocation()
+        mLocOverlay.enableFollowLocation()
+        mLocOverlay.runOnFirstFix {
+            map.overlays.clear()
+            map.overlays.add(mLocOverlay)
+        }
     }
 
     companion object {
